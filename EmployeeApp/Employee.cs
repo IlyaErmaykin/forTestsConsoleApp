a0c3a63e443c5406c06df1c0ab4fd43ba5f68ea6
Ilya Ermaykin <ermaykin95@mail.ru>
2018-04-23 23:15:39 +0400

    #10 Обзор ООП;  - Добавление get и set методов для возможности обращения к полям;

--- EmployeeApp/Employee.cs
+++ EmployeeApp/Employee.cs
@@ -14,7 +14,7 @@ namespace EmployeeApp
         private float currPay;
 
         // Создание конструкторов;
-        public Employee();
+        public Employee() { }
         public Employee(string name, int id, float pay)
         {
             empName = name;
@@ -40,6 +40,29 @@ namespace EmployeeApp
             Console.WriteLine("Name: {0}", empName);
             Console.WriteLine("ID: {0}", empID);
             Console.WriteLine("Pay: {0}", currPay);
+        }
+
+        /// <summary>
+        /// Added get name
+        /// </summary>
+        /// <returns></returns>
+        public string GetName()
+        {
+            return empName;
+        }
+
+        public void SetName(string name)
+        { 
+            //Проверка исходного значения перед добавлением
+            if (name.Length > 15)
+            {
+                //Ошибка, имя должно иметь меньше 16 символов
+                Console.WriteLine("Error, Name must be less that 16 characters...");
+            }
+            else
+            {
+                empName = name;
+            }
         }
     }
 }
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -10,9 +10,16 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
-            //Ошибка, невозможно обратиться к закрытым полям объекта;
-            Employee emp = new Employee();
-            //emp.empName = "Marv";
+            //Теперь все в порядке;
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30000);
+            emp.GiveBonus(1000);
+            emp.DisplayStatus();
+                        
+            //Использование get/set для взаимодействия с именами объектов;
+            emp.SetName ("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+            Console.ReadKey();
         }
     }
 }
