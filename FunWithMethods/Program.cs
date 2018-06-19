73132d76e3d7becc40758caafe6882b82116965f
Ilya Ermaykin <ermaykin95@mail.ru>
2018-06-19 10:01:11 +0400

    #11 Разбор стандартного поедения передачи параметнров по значению;  - Создание проекта FunWithMethods;  - Добавлние метода Add;

---
+++ FunWithMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithMethods
+{
+    class Program
+    {
+        // По умолчанию аргументы передаются по значению
+        static int Add(int x, int y)
+        {
+            int ans = x + y;
+
+            //Вызывающий код не увидит эти изменения 
+            // т.к. изменяется копия исходных данных.
+            x = 10000;
+            y = 88888;
+
+            return ans;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Fun with Method *****\n");
+
+            //Передать две переменных по значению
+            int x = 9, y = 10;
+            Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
+            Console.WriteLine("Answer is: {0}", Add(x, y));
+            Console.WriteLine("After call: X: {0}, Y: {1}", x, y);
+
+            Console.Read();
+        }
+    }
+}
